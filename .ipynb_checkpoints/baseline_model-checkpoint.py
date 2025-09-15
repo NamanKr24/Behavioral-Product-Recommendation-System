adc8299ed991c69229e88abfd1110b4cf71f738e
NamanKr24 <97843974+NamanKr24@users.noreply.github.com>
2025-09-16 01:15:38 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/baseline_model-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def get_baseline_recommendations(train_df, n=10):
-    popular_recs = train_df[train_df['event_type'] == 'purchase']['product_id'].value_counts().head(n).index.tolist()
-    return popular_recs
